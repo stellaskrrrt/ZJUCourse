ef06317b36af765cc2f5a37fbfbd9786e6e6d96b
997403593@qq.com <DU9e!ZJJYEYp93N>
2020-12-14 22:11:31 +0800

    register

--- index/views.py
+++ index/views.py
@@ -41,7 +41,7 @@ def register(request):
     elif request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
-        phone = request.POST.get('password')
+        phone = request.POST.get('phone')
         email = request.POST.get('email')
 
         query = "select * from User where User_ID = %s"
@@ -50,6 +50,6 @@ def register(request):
         if len(result) == 1:
             return HttpResponse('no')
         else:
-            sql.execute("insert into Assignment values(%s, %s, %s, null, %s, %s, %s)", username,
-                        password, username, email, phone, 0)
+            sql.execute("insert into User values(%s, %s, %s, %s, %s, %s, %s)", username,
+                        password, username,'', email, phone, 0)
             return HttpResponse('yes')
